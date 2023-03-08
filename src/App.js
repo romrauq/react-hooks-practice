f07de9c286715c48ac5c01111ae467134631fede
John Quarmor <johnequarmor@gmail.com>
2023-03-08 21:44:58 +0000

    Imported & rendered useEffect component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import UseEffect from "./components/02_useEffect/UseEffect";
 
 const App = () => {
 	return (
 		<div>
-			<h1>Hello World</h1>
+			<UseEffect />
 		</div>
 	);
 };
